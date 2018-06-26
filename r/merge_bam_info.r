a152a13c92e03d32b0735ceab421cb649d3d0f32
Rod Docking <rdocking@bcgsc.ca>
2018-06-26 16:53:59 -0700

    Add first implementation of BAM file merging script, example YAML template

--- r/merge_bam_info.r
+++ r/merge_bam_info.r
@@ -7,3 +7,22 @@ p <- add_argument(p, '--bam_info_files', type = "character",
                   nargs = Inf, help = 'BamInfo files')
 argv <- parse_args(p)
 
+# Load all the input BAM info files and merge them
+# These were all written with `write.table`
+input_paths <- argv$bam_info_files
+
+merged.df <- read.table(file = input_paths[1],
+             quote = FALSE, sep = "\t", row.names = FALSE)
+
+for (i in seq(2, length(input_paths))) {
+  print(i)
+  new.df <- read.table(file = input_paths[i],
+            quote = FALSE, sep = "\t", row.names = FALSE)
+  merged.df <- rbind(merged.df, new.df)
+  rm(new.df)
+}
+
+# Export
+output_file_name <- paste0(argv$out_prefix, '.bam_info.tsv')
+write.table(merged.df, file = output_file_name,
+            quote = FALSE, sep = "\t", row.names = FALSE)
