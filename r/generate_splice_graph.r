f73bd211669130728d156cf940a89fd4b0b776c6
Rod Docking <rdocking@bcgsc.ca>
2018-06-29 15:36:12 -0700

    Add more verbose output and handle single-sample edge-case in building splice graph

--- r/generate_splice_graph.r
+++ r/generate_splice_graph.r
@@ -22,14 +22,17 @@ print(paste("Splice graph mode:", argv$splice_graph_type))
 # Loads 'txdb_features_main_chrs'
 # This is the main transcript DB generated from the supplied GTF file
 load(argv$transcript_db)
+print("Transcript DB summary:")
+print(summary(txdb_features_main_chrs))
 
 # Get the list of txfeatures files
 txfeatures_files <- argv$txfeatures_files
+num_tx_features_files <- length(txfeatures_files)
+print(paste("tx_features files to merge: ", num_tx_features_files))
 
 # If we're using the known annotations, or both known and novel, 
 # load the known annotation set first:
 # Otherwise, start from the first set of predictions  
-
 first_sample_to_merge <- NULL
 merged <- NULL
 if (argv$splice_graph_type %in% c('known', 'both')) {
@@ -41,12 +44,20 @@ if (argv$splice_graph_type %in% c('known', 'both')) {
   rm(tx_features)
   first_sample_to_merge <- 2
 } 
+
+print("Initial splice graph:")
 print(summary(merged))
 
+# Need to handle the case where we're using 'predicted' with only 1 input file
+# Known: doesn't matter since we ignore the tx_features files
+# Both: There will always be at least one tx_features files to merge
+# Predicted: There may be only one input file 
+single_sample_predicted_check <- (argv$splice_graph_type == 'predicted' & num_tx_features_files == 1)
+
 # If we're using novel predictions, load all the new TxFeatures files and merge them
-if (argv$splice_graph_type %in% c('predicted', 'both')) {
+if (argv$splice_graph_type %in% c('predicted', 'both') & !(single_sample_predicted_check)) {
   
-  for (i in seq(first_sample_to_merge, length(txfeatures_files))) {
+  for (i in seq(first_sample_to_merge, num_tx_features_files)) {
     print(i)
     load(txfeatures_files[i])
     merged <- SGSeq::mergeTxFeatures(merged, tx_features)
@@ -70,7 +81,10 @@ merged.process_terminal.sg_features <-
 merged.process_terminal.sg_features.annotated <- 
   SGSeq::annotate(merged.process_terminal.sg_features, 
                   txdb_features_main_chrs)
-  
+
+print("Final splice graph:")
+print(summary(merged.process_terminal.sg_features.annotated))
+
 # Rename and set file name
 splice_graph_features <- merged.process_terminal.sg_features.annotated
 output_file_name <- paste0(argv$output_prefix, '.sg_features.RData')
